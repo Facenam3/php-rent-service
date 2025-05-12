70cc6cba5e41739c09d70792fcb6a9f44936c452
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-05-12 22:12:38 +0200

    added the database and app

---
+++ bin/schema_load.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . "/../bootstrap.php";
+use Core\App;
+
+$db = App::get('database');
+
+$schemaFILE = __DIR__ . "/../database/schema.sql";
+$sql = file_get_contents($schemaFILE);
+
+try {
+    $parts = array_filter(explode(separator: ";", string: $sql));
+    var_dump($parts);
+    foreach($parts as $sqlPart) {
+        $db->query($sqlPart);
+    }
+    echo "Schema loaded successfully.";
+} catch(Exception $e) {
+    echo "Error loading schema: " . $e->getMessage() . "/n";
+}
