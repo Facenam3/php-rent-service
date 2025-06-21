66e9c46d72f397d287167cbbb60617e3f57623af
Dalibor <87080924+Facenam3@users.noreply.github.com>
2025-06-21 08:11:16 +0200

    Revert "09.homepage"

--- app/Models/Reservation.php
+++
@@ -1,8 +0,0 @@
-<?php 
-namespace App\Models;
-
-use Core\Model;
-
-class Reservation extends Model {
-    protected static $table = 'reservations';
-}
--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @var Core\Router $router
- */
-
-$router->add('GET', '/','HomeController@index');
-$router->add('GET', '/cars','CarController@index');
-$router->add('GET', '/cars/{id}','CarController@show');
