3d9e999047d195c4dcf3f8287219058b18c22fb9
Dalibor Jovanovski <dalibor.jovanov2k1@gmail.com>
2025-07-03 19:12:10 +0200

    fix

--- config.php
+++ config.php
@@ -10,8 +10,8 @@ return[
         'dbname' => __DIR__ . '/database/rent.sqlite'
     ],
     'google' => [
-            'client_id' => "212135940004-u9gf5mos9sdf2j40i85a32u7cqd80pj3v3b.apps.googleusercontent.com",
-            'client_secret' => "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ylS",
-            'redirect_url' => "http://localhost:8000/auth/google/callback",
+            'client_id' => "your_client_id",
+            'client_secret' => "your_client_secret",
+            'redirect_url' => "your_redirect_url",
     ] 
 ];
